1ee36fc7349d29fdde40455e9d95a7e6bf5f94b5
smithwebtek <brad@smithwebtek.com>
2018-04-09 11:17:35 -0400

    Add analysis methods

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,14 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
 	self.abstract_class = true
-	
-	def self.my_methods
-		puts self.methods.sort
-		puts 'Total number of Class methods: ' + self.methods.count.to_s
-	end
-	
-	def my_methods
-		puts self.methods.sort
-		puts 'Total number of instance methods: ' + self.methods.count.to_s
-	end
 
 end
