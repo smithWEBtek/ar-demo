446a124951ac39b09d51faff7d6c62d36562a9a3
smithwebtek <brad@smithwebtek.com>
2018-04-09 10:23:27 -0400

    Add custom methods to compare class and instance methods before AR

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,14 @@
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
+	self.abstract_class = true
+	
+	def self.my_methods
+		puts self.methods.sort
+		puts 'Total number of Class methods: ' + self.methods.count.to_s
+	end
+	
+	def my_methods
+		puts self.methods.sort
+		puts 'Total number of instance methods: ' + self.methods.count.to_s
+	end
+
 end
